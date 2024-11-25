fa1c8268a1f9f3b7d2442b25b29cad4e469d8b8b
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-11-24 20:21:56 -0600

    finished daily_temperatures

--- stack/_tests.py
+++ stack/_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from valid_parentheses import Solution as IsValid
 from evaluate_reverse_polish_notation import Solution as EvalRPN
+from daily_temperatures import Solution as DailyTemperatures
 
 class TestValidParenthesis(unittest.TestCase):
     def test_valid_parenthesis(self):
@@ -15,6 +16,12 @@ class TestEvalRPN(unittest.TestCase):
         self.assertEqual(solution.evalRPN(["1","2","+","3","*","4","-"]), 5)
         self.assertEqual(solution.evalRPN(["2","1","+","3","*"]), 9)
         self.assertEqual(solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]), 22)
+
+class TestDailyTemperatures(unittest.TestCase):
+    def test_daily_temperatures(self):
+        solution = DailyTemperatures()
+        self.assertEqual(solution.dailyTemperatures([30,38,30,36,35,40,28]), [1,4,1,2,1,0,0])
+        self.assertEqual(solution.dailyTemperatures([22,21,20]), [0,0,0])
 
 if __name__ == "__main__":
     unittest.main()
--- stack/daily_temperatures.py
+++ stack/daily_temperatures.py
@@ -8,8 +8,17 @@ a warmer temperature appears on a future day. If there is no day in the future w
 a warmer temperature will appear for the ith day, set result[i] to 0 instead.
 '''
 
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        pass # TODO
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            stack.append(i)
+        
+        return result
