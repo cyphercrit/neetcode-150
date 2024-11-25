89b3ef594fe5abaa4183779bf24e163667a5e9ad
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-11-24 19:56:31 -0600

    Finished evalrpn

--- stack/_tests.py
+++ stack/_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from valid_parentheses import Solution as IsValid
+from evaluate_reverse_polish_notation import Solution as EvalRPN
 
 class TestValidParenthesis(unittest.TestCase):
     def test_valid_parenthesis(self):
@@ -7,6 +8,13 @@ class TestValidParenthesis(unittest.TestCase):
         self.assertTrue(solution.isValid("[]"))
         self.assertTrue(solution.isValid("([{}])"))
         self.assertFalse(solution.isValid("[(])"))
+
+class TestEvalRPN(unittest.TestCase):
+    def test_evaluate_reverse_polish_notation(self):
+        solution = EvalRPN()
+        self.assertEqual(solution.evalRPN(["1","2","+","3","*","4","-"]), 5)
+        self.assertEqual(solution.evalRPN(["2","1","+","3","*"]), 9)
+        self.assertEqual(solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]), 22)
 
 if __name__ == "__main__":
     unittest.main()
--- stack/evaluate_reverse_polish_notation.py
+++ stack/evaluate_reverse_polish_notation.py
@@ -11,8 +11,24 @@ Return the integer that represents the evaluation of the expression.
 
 '''
 
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        pass # TODO
+        stack = []
+
+        for char in tokens:
+            if char == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif char == "-":
+                x, y = stack.pop(), stack.pop()
+                stack.append(y - x)
+            elif char == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif char == "/":
+                x, y = stack.pop(), stack.pop()
+                stack.append(int(y / x))
+            else:
+                stack.append(int(char))
+
+        return stack.pop()
