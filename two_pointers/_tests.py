feaaab5df6bb18aa1f73438e7d9fc6ef607dcf55
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-11-24 18:00:10 -0600

    finished three_sum

--- two_pointers/_tests.py
+++ two_pointers/_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from valid_palindrome import Solution as ValidPalindrome
 from two_sum_2 import Solution as TwoSum
+from three_sum import Solution as ThreeSum
 
 class TestValidPalindrome(unittest.TestCase):
     def test_valid_palindrome(self):
@@ -11,7 +12,15 @@ class TestValidPalindrome(unittest.TestCase):
 class TestTwoSum(unittest.TestCase):
     def test_two_sum(self):
         solution = TwoSum()
-        self.assertEqual(solution.twoSum([1,2,3,4], 3), [1, 2])  
+        self.assertEqual(solution.twoSum([1,2,3,4], 3), [1, 2])
+
+class TestThreeSum(unittest.TestCase):
+    def test_three_sum(self):
+        solution = ThreeSum()
+        self.assertEqual(sorted(solution.threeSum([-1,0,1,2,-1,-4])), [[-1,-1,2], [-1,0,1]])
+        self.assertEqual(solution.threeSum([0,1,1]), [])
+        self.assertEqual(solution.threeSum([0,0,0]), [[0,0,0]])
+        self.assertEqual(solution.threeSum([-2,0,0,2,2]), [[-2,0,2]])
       
 if __name__ == "__main__":
     unittest.main()
--- two_pointers/three_sum.py
+++ two_pointers/three_sum.py
@@ -7,8 +7,45 @@ where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all disti
 The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
 '''
 
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass # TODO
+        first = 0
+        nums.sort()
+
+        # loops until after first, second, third are right next to eachother
+        result = []
+        while first + 2 < len(nums):
+            # skips duplicate first values
+            if first != 0 and nums[first] == nums[first-1]:
+                first += 1
+                continue
+
+            second, third = first + 1, len(nums) - 1
+
+            while second < third:
+                curr_sum = nums[first] + nums[second] + nums[third]
+
+                if curr_sum < 0:
+                    second += 1
+                    continue
+
+                if curr_sum > 0:
+                    third -= 1
+                    continue
+
+                if curr_sum == 0:
+                    result.append([nums[first], nums[second], nums[third]])
+                    # skips duplicates
+                    while second < third and nums[third] == nums[third-1]:
+                        third -= 1
+                    while second < third and nums[second] == nums[second+1]:
+                        second += 1
+                    second += 1
+                    third -= 1
+            
+            first += 1
+
+        return result
+
